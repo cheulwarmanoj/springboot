28ba7bb4cf1e78b096693864211519f40df4b031
cheulwarmanoj <cheulwar.manoj@gmail.com>
2020-04-14 22:58:42 +0530

    order management project

---
+++ Order-Management/frontend/build/precache-manifest.6dd4b259a5352d1323e1b0bf4ee595f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c68f70ff92e3c4b4d3b0c05ad0e2ae71",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3dc497c1f657d386ab63",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "b04663a900b9417b1bb9",
+    "url": "/static/js/2.9e23c9d6.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/static/js/2.9e23c9d6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3dc497c1f657d386ab63",
+    "url": "/static/js/main.b31f3cc7.chunk.js"
+  },
+  {
+    "revision": "a8139511e7e3c6f8467f",
+    "url": "/static/js/runtime-main.1c2d59f4.js"
+  }
+]);
